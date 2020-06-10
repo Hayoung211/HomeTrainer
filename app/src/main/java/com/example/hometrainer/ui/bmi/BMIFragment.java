f3644f0019c3c1d513d771bb7f5890bb0c9e738d
hayoung <sks05039@naver.com>
2020-06-10 17:06:36 +0900

    yaho

--- app/src/main/java/com/example/hometrainer/ui/bmi/BMIFragment.java
+++ app/src/main/java/com/example/hometrainer/ui/bmi/BMIFragment.java
@@ -1,22 +1,29 @@
 package com.example.hometrainer.ui.bmi;
 
+import android.app.ActionBar;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.hometrainer.MainActivity;
 import com.example.hometrainer.R;
-import com.example.hometrainer.ui.bmi_result.BMIResutFragment;
+
+import static androidx.core.content.ContextCompat.getSystemService;
 
 public class BMIFragment extends Fragment {
 
@@ -24,16 +31,23 @@ public class BMIFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         final View root = inflater.inflate(R.layout.fragment_bmi, container, false);
 
-        Button show_result = (Button) root.findViewById(R.id.bmi_result_button);
+        final InputMethodManager mInputMethodManager = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        ActionBar actionBar = ((MainActivity) getActivity()).getActionBar();
+        actionBar.show();
+
+        final Button show_result = (Button) root.findViewById(R.id.bmi_result_button);
         show_result.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 EditText get_height = (EditText) root.findViewById(R.id.bmi_height_input);
                 EditText get_weight = (EditText) root.findViewById(R.id.bmi_weight_input);
+                ImageView bmi_image = (ImageView)root.findViewById(R.id.bmi_image);
+                TextView bmi_result = (TextView)root.findViewById(R.id.bmi_result);
+                mInputMethodManager.hideSoftInputFromWindow(get_height.getWindowToken(), 0);
 
-                Integer height = Integer.parseInt(get_height.getText().toString());
-                Integer weight = Integer.parseInt(get_weight.getText().toString());
-//
+                int height = Integer.parseInt(get_height.getText().toString());
+                int weight = Integer.parseInt(get_weight.getText().toString());
+
                 float BMI = (float)weight/((float)(height*height)/10000);
 //
                 System.out.println("키는 " + height);
@@ -42,27 +56,29 @@ public class BMIFragment extends Fragment {
 
                 if(BMI <= 18.5) {
                     System.out.println("저체중 입니다.");
+                    bmi_image.setImageResource(R.drawable.less_weight);
+                    bmi_result.setText("저체중 입니다!");
                 }
                 else if(BMI > 18.5 && BMI <= 23) {
                     System.out.println("정상 입니다.");
+                    bmi_image.setImageResource(R.drawable.good_weight);
+                    bmi_result.setText("정상 입니다!");
                 }
                 else if(BMI > 23 && BMI <= 25) {
                     System.out.println("과체중 입니다.");
+                    bmi_image.setImageResource(R.drawable.more_weight);
+                    bmi_result.setText("과체중 입니다!");
                 }
                 else if(BMI > 25 && BMI <= 30) {
                     System.out.println("비만 입니다.");
+                    bmi_image.setImageResource(R.drawable.more_more_weight);
+                    bmi_result.setText("비만 입니다!");
                 }
                 else {
                     System.out.println("고도비만 입니다.");
+                    bmi_image.setImageResource(R.drawable.more_more_more_weight);
+                    bmi_result.setText("고도비만 입니다!");
                 }
-
-                Fragment fragment = new BMIResutFragment();
-                Bundle bundle = new Bundle();
-                bundle.putString("bmi", String.valueOf(BMI));
-                fragment.setArguments(bundle);
-
-                MainActivity activity = (MainActivity)getActivity();
-                activity.replaceFragment(BMIResutFragment.newInstance());
             }
         });
         return root;
---
+++ app/src/main/java/com/example/hometrainer/ui/record/RecordFragment.java
@@ -0,0 +1,21 @@
+package com.example.hometrainer.ui.record;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.hometrainer.R;
+
+public class RecordFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_record, container, false);
+
+        return root;
+    }
+}
