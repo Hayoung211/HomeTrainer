abba9e46b8fe50061538d5784dcdf6f9c23425e8
hayoung <sks05039@naver.com>
2020-06-10 20:21:42 +0900

    finish recode UI

--- app/src/main/java/com/example/hometrainer/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/hometrainer/ui/home/HomeFragment.java
@@ -1,10 +1,17 @@
 package com.example.hometrainer.ui.home;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -18,7 +25,10 @@ import com.example.hometrainer.R;
 import com.example.hometrainer.ui.challenge.ChallengeFragment;
 import com.example.hometrainer.ui.challenge_one_day.OnedayFragment;
 
-public class HomeFragment extends Fragment {
+import static android.content.Context.SENSOR_SERVICE;
+import static androidx.core.content.ContextCompat.getSystemService;
+
+public class HomeFragment extends Fragment implements SensorEventListener {
 
     private HomeViewModel homeViewModel;
 
@@ -26,12 +36,23 @@ public class HomeFragment extends Fragment {
         return new HomeFragment();
     }
 
+    TextView walk;
+    private int mSteps = 0;
+    private int mCounterSteps = 0;
+
+    private SensorManager sensorManager;
+    private Sensor stepCountSensor;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         Button challenge_more = (Button) root.findViewById(R.id.challenge_more);
+
+        sensorManager = (SensorManager)getActivity().getSystemService(Context.SENSOR_SERVICE);
+        stepCountSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        EditText walk = (EditText)root.findViewById(R.id.manbo_result);
+
         challenge_more.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -41,7 +62,42 @@ public class HomeFragment extends Fragment {
             }
         });
 
-
         return root;
+    }
+
+    public void onStart() {
+        super.onStart();
+        if(stepCountSensor != null) {
+            sensorManager.registerListener(this, stepCountSensor, SensorManager.SENSOR_DELAY_GAME);
+        }
+    }
+
+    public void onStop() {
+        super.onStop();
+        if(sensorManager != null) {
+            sensorManager.unregisterListener(this);
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType() == Sensor.TYPE_STEP_COUNTER){
+
+            //stepcountsenersor는 앱이 꺼지더라도 초기화 되지않는다. 그러므로 우리는 초기값을 가지고 있어야한다.
+            if (mCounterSteps < 1) {
+                // initial value
+                mCounterSteps = (int) event.values[0];
+            }
+            //리셋 안된 값 + 현재값 - 리셋 안된 값
+            mSteps = (int) event.values[0] - mCounterSteps;
+            walk.setText(Integer.toString(mSteps));
+            Log.i("log: ", "New step detected by STEP_COUNTER sensor. Total step count: " + mSteps );
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
